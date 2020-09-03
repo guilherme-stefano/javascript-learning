5fbb694c696692ecb119dc4b7b85d1579a703ba8
Guilherme Stéfano <guilhermenostefano@gmail.com>
2020-09-02 21:19:46 -0300

    adicionado slider resolvido

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -6,7 +6,14 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Parabéns</h1>')
+    resp.send('<h1>Parabéns usuário incluído!</h1>')
 })
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns usário alterado!</h1>')
+})
+
 
 app.listen(3003)
